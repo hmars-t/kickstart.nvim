9b956ac62066df8e9138d3692087245d9c89c1ca
Hannes Signer <signer@uni-potsdam.de>
2025-03-04 11:39:17 +0100

    add templates for jupyter support

---
+++ lua/custom/plugins/jupynium.lua
@@ -0,0 +1,15 @@
+-- return {
+--   {
+--     'kiyoon/jupynium.nvim',
+--     build = 'pip3 install --user .',
+--     -- Alternative Build-Optionen:
+--     -- build = "uv pip install . --python=$HOME/.virtualenvs/jupynium/bin/python",
+--     -- build = "conda run --no-capture-output -n jupynium pip install .",
+--   },
+--   {
+--     'rcarriga/nvim-notify', -- optional
+--   },
+--   {
+--     'stevearc/dressing.nvim', -- optional, UI für :JupyniumKernelSelect
+--   },
+-- }
