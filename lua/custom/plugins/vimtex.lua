d79fa7a79eb12b34d0caea382733044e0e8c3a30
Hannes Signer <signer@uni-potsdam.de>
2025-03-04 17:53:19 +0100

    update vimtex, snacks and lua

--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -5,6 +5,9 @@ return {
   init = function()
     -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = 'skim'
+    vim.g.vimtex_view_general_options = [[--reuse-instance]]
+    vim.g.vimtex_view_skim_sync = 1
+    vim.g.vimtex_view_skim_activate = 1
     vim.g.vimtex_complete_enabled = 1
     vim.g.vimtex_fold_enabled = 0
     vim.g.vimtex_indent_enabled = 1
